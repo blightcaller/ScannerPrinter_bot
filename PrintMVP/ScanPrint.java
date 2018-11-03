221472d1e3a0864913d43617334f286100c83869
gleps <ImpactHammer@yandex.ru>
2018-11-03 17:29:55 +0300

    fix print mvp src naming

--- PrintMVP/ScanPrint.java
+++ PrintMVP/ScanPrint.java
@@ -1,16 +1,35 @@
 package com.mycompany.spbot;
 
+import java.io.*;
 import javax.print.*;
+import javax.print.attribute.*;
+import javax.print.attribute.standard.*;
 
-public class Main {
+public class ScanPrint {
 
-    public static void main(String args[]) throws PrintException {
-        String filename;
-        if (args.length < 1) {
-            filename = "file.pdf";
-        } else {
-            filename = args[0];
+    public static void print(String filename) throws PrintException {
+        FileInputStream psStream;
+        try {
+            psStream = new FileInputStream(filename);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            return;
         }
-        ScanPrint.print(filename);
+        DocFlavor psInFormat = DocFlavor.INPUT_STREAM.PDF;
+        Doc myDoc = new SimpleDoc(psStream, psInFormat, null);
+        PrintRequestAttributeSet job_aset
+                = new HashPrintRequestAttributeSet();
+        AttributeSet lookup_aset
+                = new HashAttributeSet();
+        job_aset.add(MediaSizeName.ISO_A4);
+        job_aset.add(new PageRanges(1, 1));
+        lookup_aset.add(new PrinterName("CUPS-PDF-Printer", null));
+        PrintService[] services
+                = PrintServiceLookup.lookupPrintServices(psInFormat, lookup_aset);
+        System.out.println("Printer Selected " + services[0]);
+        if (services.length > 0) {
+            DocPrintJob job = services[0].createPrintJob();
+            job.print(myDoc, job_aset);
+        }
     }
 }
